eef58507a2d7a3e7daacc8a90408f76de9839302
Roma <romapivev@gmail.com>
2022-02-12 18:52:06 +0200

    event listener.React event. This

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/';
+// import App from './com ponents/app/';
+
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 25
+        }
+        // this.nextYear = this.nextYear.bind(this);
+    }
+    // nextYear() {
+    //     this.setState( state => ({
+    //         years: ++state.years
+    //     }))
+    // }
+    nextYear = () => { //classFields
+        this.setState( state => ({
+            years: ++state.years
+        }))
+    }
+    render() {
+        const {name, surname, link} = this.props;
+        const {years} = this.state;
+        return(
+            <>
+                <button onClick={this.nextYear}>+++</button>
+                <h1>My name is {name} {surname}, years - {years} </h1>
+                <a href={link}> My prof</a>
+            </>
+        )
+    }
+}
+
+const All = () =>{
+    return(
+        <>
+            <WhoAmI name = 'Roma' surname = 'Piven' link = 'https://cdnjs.com/libraries' />
+            <WhoAmI name = 'Sanya' surname = 'Sashka' link = 'https://cdnjs.com/libraries' />
+            <WhoAmI name = 'WHO?' surname = 'SIRMASRS' link = 'https://cdnjs.com/libraries' />
+        </>
+    )
+}
+
 
 ReactDOM.render(
-    <App />, document.getElementById('root')
+    <All/>, document.getElementById('root')
 );
 
 
