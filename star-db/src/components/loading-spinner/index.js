a70baebf19f75f33abca1cddc4987d46af53d3f3
Roma <romapivev@gmail.com>
2022-03-17 01:27:30 +0200

    Add spinner to planet loading

---
+++ star-db/src/components/loading-spinner/index.js
@@ -0,0 +1,3 @@
+import Spinner from './loading-spinner'
+
+export default Spinner;
---
+++ star-db/src/components/loading-spinner/loading-spiner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './loading-spinner.css';
+
+const Spinner = () => {
+  return(
+      <div className="spinner-wrapper">
+          <div className="loadingio-spinner-double-ring-jjvu7i419bh">
+              <div className="ldio-ygm6a1pq3g">
+                  <div></div>
+                  <div></div>
+                  <div>
+                      <div></div>
+                  </div>
+                  <div>
+                      <div></div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default Spinner;
